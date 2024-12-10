9f3c7410e09c4f4bcc8b27d67e70a808a8eefe0e
Emil Widlund <hello@emilwidlund.com>
2024-12-10 21:40:09 +0100

    fix

--- src/polar.ts
+++ src/polar.ts
@@ -2,9 +2,13 @@ import { Polar } from "@polar-sh/sdk";
 import fetch, { Headers, Request, Response } from "node-fetch";
 
 if (!globalThis.fetch) {
+  // @ts-expect-error Ensure global
   globalThis.fetch = fetch;
+  // @ts-expect-error Ensure global
   globalThis.Headers = Headers;
+  // @ts-expect-error Ensure global
   globalThis.Request = Request;
+  // @ts-expect-error Ensure global
   globalThis.Response = Response;
 }
 
